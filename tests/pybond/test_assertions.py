929c84e5c59e3e94fa8c5d576208549c046ec7fb
Guillaume Pelletier <guigui.p@gmail.com>
2023-01-22 21:22:41 -0800

    Test called_exactly_once_with_args

--- tests/pybond/test_assertions.py
+++ tests/pybond/test_assertions.py
@@ -3,6 +3,7 @@ import pytest
 import sample_code.my_module as my_module
 import sample_code.other_package as other_package
 from pybond import (
+    called_exactly_once_with_args,
     called_with_args,
     spy,
     stub,
@@ -58,6 +59,11 @@ def test_times_called_throws_on_unspied_functions():
 def test_called_with_args():
     # When there is only one function call
     with spy([other_package, "make_a_network_request"]):
+        assert not called_with_args(
+            other_package.make_a_network_request,
+            args=[42, 12],
+            kwargs={"y": "y", "other_arg": True},
+        )
         other_package.make_a_network_request(42, 12, y="y", other_arg=True)
         assert called_with_args(
             other_package.make_a_network_request,
@@ -102,6 +108,65 @@ def test_called_with_args_throws_on_unspied_functions():
     other_package.make_a_network_request(42, 12, y="y", other_arg=True)
     with pytest.raises(Exception) as e:
         called_with_args(
+            other_package.make_a_network_request,
+            args=["this doesn't matter"],
+            kwargs={"nor": "does this"},
+        )
+    assert e.value.args[0].startswith("The argument is not a spied function.")
+
+
+def test_called_exactly_once_with_args():
+    # When there is only one function call
+    with spy([other_package, "make_a_network_request"]):
+        assert not called_exactly_once_with_args(
+            other_package.make_a_network_request,
+            args=[42, 12],
+            kwargs={"y": "y", "other_arg": True},
+        )
+        other_package.make_a_network_request(42, 12, y="y", other_arg=True)
+        assert called_exactly_once_with_args(
+            other_package.make_a_network_request,
+            args=[42, 12],
+            kwargs={"y": "y", "other_arg": True},
+        )
+
+    # When there are multiple function calls (order doesn't matter)
+    with spy([other_package, "make_a_network_request"]):
+        other_package.make_a_network_request(0, y=None)
+        other_package.make_a_network_request(0, y="elephant")
+        other_package.make_a_network_request(42, 12, y="y", other_arg=True)
+        other_package.make_a_network_request(0, y="giraffe")
+        assert not called_exactly_once_with_args(
+            other_package.make_a_network_request,
+            args=[0],
+            kwargs={"y": "elephant"},
+        )
+        assert not called_exactly_once_with_args(
+            other_package.make_a_network_request,
+            args=[0],
+            kwargs={"y": "giraffe"},
+        )
+        assert not called_exactly_once_with_args(
+            other_package.make_a_network_request,
+            args=[0],
+            kwargs={"y": None},
+        )
+        assert not called_exactly_once_with_args(
+            other_package.make_a_network_request,
+            args=[42, 12],
+            kwargs={"y": "y", "other_arg": True},
+        )
+        assert not called_exactly_once_with_args(
+            other_package.make_a_network_request,
+            args=[0],
+            kwargs={"y": "sloth"},
+        )
+
+
+def test_called_exactly_once_with_args_throws_on_unspied_functions():
+    other_package.make_a_network_request(42, 12, y="y", other_arg=True)
+    with pytest.raises(Exception) as e:
+        called_exactly_once_with_args(
             other_package.make_a_network_request,
             args=["this doesn't matter"],
             kwargs={"nor": "does this"},
