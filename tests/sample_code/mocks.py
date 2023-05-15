c0cae7fdb4fe369f37e27c52e24f2e2f5173f515
Guillaume Pelletier <guigui.p@gmail.com>
2023-05-14 19:36:16 -0700

    F

--- tests/sample_code/mocks.py
+++ tests/sample_code/mocks.py
@@ -18,18 +18,6 @@ def mock_make_a_network_request(
 
 def create_mock_datetime(mock_now):
     class MockDatetime(datetime):
-        day = mock_now.day
-        fold = mock_now.fold
-        hour = mock_now.hour
-        max = mock_now.max
-        microsecond = mock_now.microsecond
-        min = mock_now.min
-        minute = mock_now.minute
-        month = mock_now.month
-        resolution = mock_now.resolution
-        second = mock_now.second
-        tzinfo = mock_now.tzinfo
-        year = mock_now.year
 
         @classmethod
         def now(cls, tz=None):
